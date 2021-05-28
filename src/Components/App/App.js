c64eecf663e95322d2a4c4e814220894eead0154
nikita gromov <gromoff332@gmail.com>
2021-05-28 16:03:50 +0500

    add form

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,18 @@
 import { useState } from "react";
 import Calendar from "react-calendar";
+import { Field, Formik, Form } from "formik";
 import "react-calendar/dist/Calendar.css";
 import "./AppStyle.css";
+import * as Yup from "yup";
+import { addDay } from "../../Redux/AppReducer";
+import { useDispatch, useSelector } from "react-redux";
 
 const App = () => {
     const [value, onChange] = useState(new Date());
+    const [isForm, setIsForm] = useState(false);
+    const formShow = () => {
+        setIsForm((prev) => !prev);
+    };
     return (
         <>
             <section className="calendar">
@@ -12,8 +20,13 @@ const App = () => {
                     <h1 className="calendar__title">Календарь событий</h1>
                     <div className="calendar__wrapper">
                         <div className="calendar__item">
-                            <Calendar onChange={onChange} value={value} className="calendar__calendar" />
-                            <button className="calendar__add calendar__button">Добавить</button>
+                            <div className={`calendar__calendarWrapper ${isForm ? "calendar__calendarWrapper--disable" : ""}`}>
+                                <Calendar onChange={onChange} value={value} className="calendar__calendar" />
+                                <button className="calendar__add calendar__button" onClick={formShow}>
+                                    Добавить
+                                </button>
+                            </div>
+                            {isForm && <CalendarForm day={value} setIsForm={setIsForm} />}
                         </div>
                         <div className="calendar__item">
                             <div className="calendar__dayDesc">
@@ -41,6 +54,93 @@ const App = () => {
                 </div>
             </section>
         </>
+    );
+};
+
+const CalendarForm = ({ day, setIsForm }) => {
+    const ddd = useSelector((state) => state.AppReducer.days);
+    const dispatch = useDispatch();
+    const [type, setType] = useState({
+        name: "Мероприятия",
+        secondInput: "Куда идти",
+        secondInputTwo: "Во сколько",
+    });
+    const ValidSchema = Yup.object().shape({
+        name: Yup.string().required("Required"),
+        secondOne: Yup.string().required("Required"),
+        secondTwo: Yup.string().required("Required"),
+    });
+    const typeChange = (e) => {
+        const type = e.target.value;
+        if (type === "event")
+            setType({
+                name: "Мероприятия",
+                secondInput: "Куда идти",
+                secondInputTwo: "Во сколько",
+            });
+        else if (type === "holidays") setType({ name: "Праздничные дни", secondInput: "Бюджет", secondInputTwo: "" });
+        else setType({ name: "Другое", secondInput: "Описание", secondInputTwo: "" });
+    };
+
+    const closeForm = () => {
+        setIsForm(false);
+    };
+    console.log(ddd);
+
+    const submitForm = (values) => {
+        dispatch(
+            addDay({
+                day: day,
+                name: values.name,
+                type: type.name,
+                secondOne: values.secondOne,
+                secondTwo: values.secondTwo,
+            })
+        );
+    };
+    return (
+        <div className="calendar__form form">
+            <h2 className="form__title">Добавить событие</h2>
+            <Formik initialValues={{ name: "", type: "", secondOne: "", secondTwo: "" }} validationSchema={ValidSchema} onSubmit={submitForm}>
+                {({ values, errors, touched }) => (
+                    <Form className="form__Wrapper">
+                        <div className="form__inutItem">
+                            <span className="form__inputTitlte">Название события</span>
+                            <Field type="text" name="name" className="form__input" />
+                        </div>
+                        <div className="form__inutItem">
+                            <span className="form__inputTitlte">Тип события</span>
+                            <select className="form__type" onChange={typeChange}>
+                                <option value="event">Мероприятия</option>
+                                <option value="holidays">Праздничные дни</option>
+                                <option value="other">Другое</option>
+                            </select>
+                        </div>
+                        {type.secondInput && (
+                            <div className="form__inutItem">
+                                <span className="form__inputTitlte">{type.secondInput}</span>
+                                <Field type="secondOne" name="secondOne" className="form__input" />
+                            </div>
+                        )}
+                        {type.secondInputTwo && (
+                            <div className="form__inutItem">
+                                <span className="form__inputTitlte">{type.secondInputTwo}</span>
+                                <Field type="secondTwo" name="secondTwo" className="form__input" />
+                            </div>
+                        )}
+
+                        <div className="form__buttonWrapp">
+                            <button className="form__button" onClick={closeForm}>
+                                Отмена
+                            </button>
+                            <button type="submit" className="form__button">
+                                Сохранить
+                            </button>
+                        </div>
+                    </Form>
+                )}
+            </Formik>
+        </div>
     );
 };
 
--- src/Redux/AppReducer.js
+++ src/Redux/AppReducer.js
@@ -1,13 +1,20 @@
 let initialState = {
-    init: false,
-    genre: [],
+    days: [],
 };
+const APP_ADD_DAY = "APP_ADD_DAY";
 
 const AppReduser = (state = initialState, action) => {
     switch (action.type) {
+        case APP_ADD_DAY:
+            return { ...state, days: [...state.days, action.data] };
         default:
             return state;
     }
 };
+
+export const addDay = (data) => ({
+    type: "APP_ADD_DAY",
+    data,
+});
 
 export default AppReduser;
